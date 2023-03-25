046870f2e58b795cc8465a600c3f8faf3c45649a
yara <yara@yahoo.com>
2023-03-24 18:07:26 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int num, i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + 48);
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
 		_putchar('\n');
 	}
