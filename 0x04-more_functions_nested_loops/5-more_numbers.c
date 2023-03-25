a5e618ef0577f64c8e687d0e4078093b9faab1c4
yara <yara@yahoo.com>
2023-03-24 18:09:54 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - print 0 - 14
- *      10 times
+ * more_numbers - print 0 - 14 ten times and you
+ *      can only use _putchar three times
  *
  * Return: Always 0 (Success)
 */
