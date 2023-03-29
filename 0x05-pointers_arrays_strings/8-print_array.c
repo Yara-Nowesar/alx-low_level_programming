c7a1e69d05ec2d9853c5121853aad710cec77384
yara <yara@yahoo.com>
2023-03-28 18:59:31 -0700

    a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,24 @@
 #include "main.h"
+
+/**
+ * print_array - prints a elements of an array of integers
+ *
+ * @a: string parameter input
+ * @n: elements parameter input
+ *
+ * Return: nothing
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
