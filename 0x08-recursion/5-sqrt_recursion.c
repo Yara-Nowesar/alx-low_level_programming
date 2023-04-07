52cfc96426d4f54db5e4e15c96e7fcd3167bb32a
yara <yara@yahoo.com>
2023-04-06 19:47:17 -0700

    a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,25 @@
 #include "main.h"
 
 /**
- * */
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: int
+ * @val: square root
+ * Return: int
+*/
+
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (sqare(n - 1));
+}
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
+
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,26 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * */
+ * is_prime_number - return 1 if the number is prime
+ * @othrn: int
+ * @n: number
+ * @Return: 0 or 1
+*/
+
+int check_prime(int n, int othrn);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
+int check_prime(int n,int othrn)
+{
+	if (othrn >= n && n > 1)
+		return (1);
+	else if (n % othrn == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, othrn + 1));
+}
+
